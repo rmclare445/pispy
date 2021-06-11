ce8e816bf52cdfd346960477e5147b4821b01666
rmclare445 <52092909+rmclare445@users.noreply.github.com>
2021-06-10 19:46:43 -0700

    Update
    Add constant operation functionality and receiving functionality

--- camera.py
+++ camera.py
@@ -3,3 +3,7 @@ import os
 def take_photo( output ):
 	
 	os.system( 'raspistill -o %s' % output )
+    
+def delete_trigger( trigger ):
+    
+    os.system( 'rm ' + trigger )
--- pispy.py
+++ pispy.py
@@ -3,17 +3,19 @@ import glob
 import camera as c
 
 outdir = "/home/ryan/photos/"
-'''
+
 trigger = False
 
 while True:
     
-    if trigger:
-'''        
-lt = time.localtime()
+    if "click" in glob.glob( '/home/ryan/receive/*' ):
+        
+        lt = time.localtime()
 
-outname = "%02d%02d%02d_%02d%02d%04d.jpg" % (lt[3], lt[4], lt[5], lt[2], lt[1], lt[0])
+        outname = "%02d%02d%02d_%02d%02d%04d.jpg" % (lt[3], lt[4], lt[5], lt[2], lt[1], lt[0])
 
-output = outdir + outname
+        output = outdir + outname
 
-c.take_photo( output )
+        c.take_photo( output )
+        
+        c.delete_trigger( '/home/ryan/receive/' )
