baf11b5af2875c9b02eafe6e702ee3aa92b39a05
rmclare445 <52092909+rmclare445@users.noreply.github.com>
2021-06-10 19:55:59 -0700

    Debugging

--- pispy.py
+++ pispy.py
@@ -10,6 +10,8 @@ while True:
     
     if "click.txt" in glob.glob( '/home/ryan/*' ):
         
+        print(glob.glob( '/home/ryan/*' ))
+        
         lt = time.localtime()
 
         outname = "%02d%02d%02d_%02d%02d%04d.jpg" % (lt[3], lt[4], lt[5], lt[2], lt[1], lt[0])
@@ -19,3 +21,5 @@ while True:
         c.take_photo( output )
         
         c.delete_trigger( '/home/ryan/click.txt' )
+        
+        time.sleep(2)
