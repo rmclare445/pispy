6c7f6d7e3915e0bd7ba0527b4efaf0933035c47d
rmclare445 <52092909+rmclare445@users.noreply.github.com>
2021-06-10 19:59:23 -0700

    Update pispy.py
    Fixed reception issue

--- pispy.py
+++ pispy.py
@@ -2,15 +2,14 @@ import time
 import glob
 import camera as c
 
-outdir = "/home/ryan/photos/"
+homedir = "/home/ryan/"
+outdir  = "%sphotos/" % homedir
 
 trigger = False
 
 while True:
     
-    print(glob.glob( '/home/ryan/*' ))
-    
-    if "click.txt" in glob.glob( '/home/ryan/*' ):
+    if ( ("%sclick.txt" % homedir) in glob.glob( '/home/ryan/*' ) ):
         
         lt = time.localtime()
 
@@ -20,6 +19,4 @@ while True:
 
         c.take_photo( output )
         
-        c.delete_trigger( '/home/ryan/click.txt' )
-        
-    time.sleep(2)
+        c.delete_trigger( '%sclick.txt' % homedir )
