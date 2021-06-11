8d2edecc3d01208e03b168e10a22c52983d7ae9d
rmclare445 <52092909+rmclare445@users.noreply.github.com>
2021-06-10 19:37:49 -0700

    Update pispy.py
    Add photo format

--- pispy.py
+++ pispy.py
@@ -12,7 +12,7 @@ while True:
 '''        
 lt = time.localtime()
 
-outname = "%02d%02d%02d_%s%s%s" % (lt[3], lt[4], lt[5], lt[2], lt[1], lt[0])
+outname = "%02d%02d%02d_%s%s%s.jpg" % (lt[3], lt[4], lt[5], lt[2], lt[1], lt[0])
 
 output = outdir + outname
 
